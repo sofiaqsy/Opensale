e0344a01ea9153f2fd542593fc0661ccdf150073
DxrosAnys <diego.267.ri@gmail.com>
2017-03-16 16:44:09 -0500

    hola

--- html/overall/topnav.php
+++ html/overall/topnav.php
@@ -13,29 +13,28 @@
                         <div class="mbr-navbar__column"><ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-decorator mbr-buttons--active"><li class="mbr-navbar__item">
                           <a class="mbr-buttons__link btn text-white" href="#">INICIO</a></li><li class="mbr-navbar__item">
                             <?php
-                          if(!isset($_SESSION['app_id'])) {
+                                  if(!isset($_SESSION['app_id'])) {
                             echo '<a class="mbr-buttons__link btn text-white" data-toggle="modal" data-target="#Login">INICIAR SESIÓN</a></li><li class="mbr-navbar__item">
                             <div class="mbr-navbar__column">
                             <ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-inverse mbr-buttons--active">
                               <li class="mbr-navbar__item"><a class="mbr-buttons__btn btn btn-danger" data-toggle="modal" data-target="#Registro">REGISTRO</a></li>
                             </ul>
                             </div>';
-                          } else {
+                                  }else{
                             echo '
                             <div class="mbr-navbar__column"><ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-inverse mbr-buttons--active"><li class="mbr-navbar__item"><a class="mbr-buttons__btn btn btn-danger" href="?view=perfil&id='.$_SESSION['app_id'].'">'. strtoupper($_users[$_SESSION['app_id']]['NOM_USU']) .'</a></li></ul></div>
                             <div class="mbr-navbar__column"><ul class="mbr-navbar__items mbr-navbar__items--right mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-inverse mbr-buttons--active"><li class="mbr-navbar__item"><a class="mbr-buttons__btn btn btn-primary" href="?view=logout">SALIR</a></li></ul></div>
                           ';
                           }
                           ?>
+                        </div>
                     </nav>
                 </div>
             </div>
         </div>
-    </div>
 </section>
+
 <?php
-
-
 if(!isset($_SESSION['app_id'])) {
   include(HTML_DIR . '/public/login.html');
   include(HTML_DIR . '/public/reg.html');
